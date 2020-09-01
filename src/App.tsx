5b275ca6af8c473e0ae011e78be030631cc5fc3e
Michael <mklosterboer@gmail.com>
2020-09-01 17:24:55 -0500

    Initial layout and remove extra css

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Planning Poker</h1>
       </header>
+      <div id="main">
+        <div id="create-session" className="main-section">
+          <h2>
+            Create a new session
+          </h2>
+        </div>
+        <div className="vertical-rule"></div>
+        <div id="join-session" className="main-section">
+          <h2>
+            Join an existing session
+          </h2>
+        </div>
+      </div>
     </div>
   );
 }
